1845602f01526b49eac7d73dfdcc48a78cec38ec
Rioba-Ian <riobafelix@gmail.com>
2025-07-16 17:58:09 +0300

    init project, set up favicon and logo from figma

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const georgiaFont = localFont({
+ src: "../fonts/georgia-2/georgia.ttf",
+ variable: "--font-georgia",
+});
+
+export const openSans = Open_Sans({
+ subsets: ["latin"],
+ variable: "--font-open-sans",
+});
+
+export const metadata: Metadata = {
+ title: "Rotaract Club of Hurlingham",
+ description:
+  "Rotaract Club of Hurlingham is a community service club for young professionals in Nairobi, Kenya.",
+};
+
+export default function RootLayout({
+ children,
+}: Readonly<{
+ children: React.ReactNode;
+}>) {
+ return (
+  <html lang="en">
+   <body className={`${openSans.className} antialiased`}>{children}</body>
+  </html>
+ );
+}
