c135a66e96fb8c5dcb3853b772bd41560df5cd2d
Rioba-Ian <riobafelix@gmail.com>
2025-08-26 12:14:31 +0300

    add sentry plugin to cms project

--- rac-hurlingham-cms/config/plugins.ts
+++ rac-hurlingham-cms/config/plugins.ts
@@ -8,6 +8,13 @@ module.exports = ({ env }) => ({
     api_key: env("CLOUDINARY_API_KEY"),
     api_secret: env("CLOUDINARY_API_SECRET"),
    },
+   sentry: {
+    enabled: true,
+    config: {
+     dsn: env("NODE_ENV") === "production" ? env("SENTRY_DSN") : null,
+     sendMetadata: true,
+    },
+   },
    actionOptions: {
     upload: {},
     uploadStream: {},
