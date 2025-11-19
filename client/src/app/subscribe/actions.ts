be2edb4b9d8a8ca4b0fce12a736781d18fd467db
Rioba-Ian <riobafelix@gmail.com>
2025-11-19 20:09:08 +0300

    fix: handle subscriber sign ups

--- client/src/app/subscribe/actions.ts
+++ client/src/app/subscribe/actions.ts
@@ -1,17 +1,32 @@
 "use server";
 
-export async function subscribe(email: string) {
- const response = await fetch("https://api.kit.com/v4/subscribers", {
-  method: "POST",
-  headers: {
-   "Content-Type": "application/json",
-   "X-Kit-Api-Key": process.env.KIT_API_KEY_V4!,
-  },
-  body: JSON.stringify({
-   email_address: email,
-   state: "inactive", // Important for double opt-in
-  }),
- });
+export async function subscribe(email: string, firstName: string) {
+  const response = await fetch("https://api.kit.com/v4/subscribers", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Kit-Api-Key": process.env.KIT_API_KEY_V4!,
+    },
+    body: JSON.stringify({
+      email_address: email,
+      first_name: firstName,
+      state: "active", // Important for double opt-in
+    }),
+  });
+  await fetch(`https://api.kit.com/v4/forms/8482588/subscribers`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Kit-Api-Key": process.env.KIT_API_KEY_V4!,
+    },
+    body: JSON.stringify({
+      email_address: email,
+    }),
+  });
 
- return response.json();
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
 }
