9a9a5897a452bda61b30a1e2bc74b0384f80da3a
Rioba-Ian <riobafelix@gmail.com>
2026-06-29 15:46:53 +0300

    upload dist to be used by deployment already prebuilt

---
+++ rac-hurlingham-cms/dist/src/api/board-of-director/services/board-of-director.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * board-of-director service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::board-of-director.board-of-director');
---
+++ rac-hurlingham-cms/dist/src/api/gallery-album/controllers/gallery-album.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * gallery-album controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::gallery-album.gallery-album');
